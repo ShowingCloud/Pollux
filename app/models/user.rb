febbc2718fd0e79a8cb49186515389dfbb9639c8
WANG Guoqin <wgq@showingcloud.com>
2014-08-21 10:48:41 +0000

    old user scaffold removed

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  include PasswordCrypt, GenerateUuid
-
-  before_create :generate_uuid
-  has_many :addresses, dependent: :destroy
-  accepts_nested_attributes_for :addresses
-
-end
--- app/views/users/show.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! @user, :id, :username, :password, :email, :created_at, :updated_at
-json.addresses @user.addresses, :id, :user_id, :address, :balance, :uuid, :created_at, :updated_at
