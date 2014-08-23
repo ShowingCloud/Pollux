4442bf1eaa11e58ec3da2232de48e5b91cea9d54
WANG Guoqin <wangguoqin1001@gmail.com>
2014-08-23 16:52:54 +0800

    submit.js: alert when empty

--- app/assets/javascripts/submit.js
+++ app/assets/javascripts/submit.js
@@ -1,6 +1,7 @@
 $(document).ready (function() {
   $("body").delegate ("form", "submit", function() {
     if ($("#address_nickname").val() == "" || $("#address_email").val() == "" || $("#address_video").val() == "") {
+      alert("请填写完整信息");
       event.preventDefault();
     }
   });
