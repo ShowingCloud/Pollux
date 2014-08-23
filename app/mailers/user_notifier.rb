9fefd677bff83ee1936b9a99d20b220a854b3ab9
WANG Guoqin <wgq@showingcloud.com>
2014-08-23 07:23:44 +0000

    mailer: bug fix and environment

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,15 +1,15 @@
 class UserNotifier < ActionMailer::Base
   default from: "contact@antshares.org"
 
-  def send_signup_email(user)
-    @user = user
-    mail( :to => @user.email,
+  def send_signup_email(address)
+    @address = address
+    mail( :to => @address.email,
           :subject => 'Thanks for signing up Antshares' )
   end
 
-  def send_notification(user)
-    @user = user
-    mail( :to => @user.email,
+  def send_notification(address)
+    @address = address
+    mail( :to => @address.email,
           :subject => 'Someone donated' )
   end
 
